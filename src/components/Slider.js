c3143496b713b05af106abc0874fc8e9bb5c36a0
ToppleKek <topkek@topplekek.xyz>
2023-03-07 20:34:08 -0500

    Slider: Allow dragging outside of slider body and add label

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,25 +1,42 @@
-import { Component } from "react";
+import { Component, createRef } from "react";
 import './style/Slider.css';
 
 export default class Slider extends Component {
     constructor(props) {
         super(props);
 
+        this.slider_ref = createRef(null);
+        document.addEventListener('mousemove', this.on_move);
+        document.addEventListener('mouseup', this.on_mouse_up);
         this.state = {
             value: props.value,
             down: false
         };
     }
 
+    componentWillUnmount() {
+        document.removeEventListener('mousemove', this.on_move);
+        document.removeEventListener('mouseup', this.on_mouse_up);
+    }
+
     update_slider_value(client_x, rect) {
         const x = client_x - rect.left;
-        const value = (x / rect.width) * (this.props.max - this.props.min) + this.props.min;
+        let value = Math.trunc(((x / rect.width) * (this.props.max - this.props.min) + this.props.min) * 100) / 100;
+
+        if (value > this.props.max)
+            value = this.props.max;
+        else if (value < this.props.min)
+            value = this.props.min;
+
         this.props.onInput(value);
         this.setState({ value });
     }
 
     on_mouse_down = (e) => {
-        this.update_slider_value(e.clientX, e.target.getBoundingClientRect());
+        e.preventDefault();
+        e.stopPropagation();
+        const r = e.target.getBoundingClientRect();
+        this.update_slider_value(e.clientX, r);
         this.setState({ down: true });
     };
 
@@ -28,15 +45,17 @@ export default class Slider extends Component {
     };
 
     on_move = (e) => {
-        if (this.state.down)
-            this.update_slider_value(e.clientX, e.target.getBoundingClientRect());
+        if (this.state.down) {
+            const r = this.slider_ref.current.getBoundingClientRect();
+            this.update_slider_value(e.pageX, r);
+        }
     };
 
     render() {
         return (
-            <div className='slider-wrapper' onMouseMove={this.on_move} onMouseUp={this.on_mouse_up} onMouseDown={this.on_mouse_down} onMouseLeave={this.on_mouse_up} style={{background: `linear-gradient(to right, var(--colour-interactable) ${this.state.value / (this.props.max - this.props.min) * 100}%, var(--colour-grid_border) 1px)`}}>
-                <div className='slider-body'></div>
-                <span className='slider-text'>{this.state.value}</span>
+            <div className='slider-wrapper' ref={this.slider_ref} onMouseDown={this.on_mouse_down} style={{ background: `linear-gradient(to right, var(--colour-interactable) ${this.state.value / (this.props.max - this.props.min) * 100}%, var(--colour-grid_border) 1px)` }}>
+                <div className='slider-body' />
+                <span className='slider-text'>{this.props.label} - {this.state.value}</span>
             </div>
         );
     }
